676a0a7f0a9a92f15aefacb98b5bae882decc307
mukhammad-mz <mukhammed_tj@mail.ru>
2022-09-05 11:12:08 +0500

    fix2

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -7,44 +7,45 @@ using (StreamReader reader = new StreamReader(path))
 {
     string? line;
     int count = 0;
+
     while ((line = await reader.ReadLineAsync()) != null)
     {
-        int i = 0;
-        var lineLisr = line.Split("-", StringSplitOptions.None);
-        var ipList = lineLisr[0].Split(".", StringSplitOptions.None);
-        var ipList2 = lineLisr[1].Split(".", StringSplitOptions.None);
-
-        int ip1_3 = Convert.ToInt16(ipList[2 + i]);
-        int ip1_4 = Convert.ToInt16(ipList[3]);
-
-        int ip2_3 = Convert.ToInt16(ipList2[2 + i]);
-        int ip2_4 = Convert.ToInt16(ipList2[3]);
-
-        using (StreamWriter writer = new StreamWriter(path2, true))
+        if (line != null)
         {
+            int i = 0;
+            var lineLisr = line.Split("-", StringSplitOptions.None);
+            var ipList = lineLisr[0].Split(".", StringSplitOptions.None);
+            var ipList2 = lineLisr[1].Split(".", StringSplitOptions.None);
 
-            for (; ip2_3 >= ip1_3; ip1_3++)
+            int ip1_3 = Convert.ToInt16(ipList[2 + i]);
+            int ip1_4 = Convert.ToInt16(ipList[3]);
+
+            int ip2_3 = Convert.ToInt16(ipList2[2 + i]);
+            int ip2_4 = Convert.ToInt16(ipList2[3]);
+
+            using (StreamWriter writer = new StreamWriter(path2, true))
             {
-                if (ip2_3 == ip1_3 && ip2_4 > 0)
-                    for (int j = ip1_4; j <= ip2_4; j++)
-                    {
-                        await writer.WriteLineAsync($"{ipList[0]}.{ipList[1]}.{ip1_3}.{j}");
-                        count++;
+                for (; ip2_3 >= ip1_3; ip1_3++)
+                {
+                    if (ip2_3 == ip1_3 && ip2_4 > 0)
+                        for (int j = ip1_4; j <= ip2_4; j++)
+                        {
+                            await writer.WriteLineAsync($"{ipList[0]}.{ipList[1]}.{ip1_3}.{j}");
+                            count++;
 
-                    }
-                else
-                    for (int j = 0; j <= 255; j++)
-                    {
-                        await writer.WriteLineAsync($"{ipList[0]}.{ipList[1]}.{ip1_3}.{j}");
-                        count++;
-                    }
+                        }
+                    else
+                        for (int j = 0; j <= 255; j++)
+                        {
+                            await writer.WriteLineAsync($"{ipList[0]}.{ipList[1]}.{ip1_3}.{j}");
+                            count++;
+                        }
+
+                writer.Close();
             }
-
-            await writer.WriteLineAsync($"Count = {count}");
+            Console.WriteLine(count);
+            count = 0;
         }
-        Console.WriteLine(count);
-        count = 0;
-
     }
     reader.Close();
 }

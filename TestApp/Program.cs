1088f5b914f149bf7f724eacacdce35737db20e6
mukhammad-mz <mukhammed_tj@mail.ru>
2022-09-05 14:27:21 +0500

    json convert

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,27 +1,55 @@
-﻿string path = "C:/Users/I_Kodirov/Desktop/ip adres list.txt";
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+string path = "C:/Users/I_Kodirov/Desktop/ip adres list.txt";
 string path2 = "C:/Users/I_Kodirov/Desktop/json.txt";
 
 
 // асинхронное чтение
-using (StreamReader reader = new StreamReader(path))
+//using (StreamReader reader = new StreamReader(path))
+//{
+//    string? line;
+
+//    while ((line = await reader.ReadLineAsync()) != null)
+//    {
+//        if (line != null)
+//        {
+//            var lineLisr = line.Split("-", StringSplitOptions.None);
+//            var ipList = lineLisr[0].Split(".", StringSplitOptions.None);
+//            var ipList2 = lineLisr[1].Split(".", StringSplitOptions.None);
+
+//            using (StreamWriter writer = new StreamWriter(path2, true))
+//            {
+//                 await writer.WriteLineAsync($" {{ \"start\":\"{lineLisr[0]}\",\"end\":\"{lineLisr[1]}\" }},");
+//                writer.Close();
+//            }
+//        }
+//    }
+//    reader.Close();
+//    Console.WriteLine("<_< Complate >_>");
+//}
+
+using (FileStream fs = new FileStream(path2, FileMode.OpenOrCreate))
 {
-    string? line;
+    IpAddress? person = await JsonSerializer.DeserializeAsync<IpAddress>(fs);
 
-    while ((line = await reader.ReadLineAsync()) != null)
-    {
-        if (line != null)
-        {
-            var lineLisr = line.Split("-", StringSplitOptions.None);
-            var ipList = lineLisr[0].Split(".", StringSplitOptions.None);
-            var ipList2 = lineLisr[1].Split(".", StringSplitOptions.None);
+    Console.WriteLine(person.IpAdresList.Count);
+}
 
-            using (StreamWriter writer = new StreamWriter(path2, true))
-            {
-                 await writer.WriteLineAsync($" {{ \"start\":\"{lineLisr[0]}\",\"end\":\"{lineLisr[1]}\" }},");
-                writer.Close();
-            }
-        }
-    }
-    reader.Close();
-    Console.WriteLine("<_< Complate >_>");
+
+
+public partial class IpAddress
+{
+    [JsonPropertyName("ip-adres-list")]
+    public List<IpAdresList> IpAdresList { get; set; }
+}
+
+public partial class IpAdresList
+{
+    [JsonPropertyName("start")]
+    public string Start { get; set; }
+
+    [JsonPropertyName("end")]
+    public string End { get; set; }
 }

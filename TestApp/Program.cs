539a4671d97364cf6d6b5910a2851d7115f1dd42
Mukhammad-mz <mukhammed_tj@mail.ru>
2022-11-16 11:36:58 +0500

    year

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 200; i++)
+{
+    Console.WriteLine($"{i} {TestYear.GetExpirationFormatYear(i)}");
+}
+class  TestYear
+{
+    public static string GetExpirationFormatYear(int expirationDate)
+    {
+        int expiration = expirationDate % 10;
+
+        if (expiration == 1)
+            return "год";
+
+        if (expiration >= 2 && expiration <= 4)
+            return "года";
+
+        if (expiration >= 5 && expiration <= 9 || expiration == 0)
+            return "лет";
+
+        return "";
+    }
+}
